089f25ebff29efdd874eda8695d98e72c6638e7c
garrettbearss <101658141+garrettbearss@users.noreply.github.com>
2022-04-21 18:10:19 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,124 +1,206 @@
 // Github Link: https://github.com/garrettbearss/Banker-s-Algorithm/edit/main/main.cpp
 // Author: Garrett Bearss
-// Date: 4/19/2022
+// Date: 4/21/2022
 // main.cpp
-// To run use gcc main.cpp -lstdc++
-// then ./a.out
+// To run use the following commands:
+// $ g++ main.cpp -o main
+// ./main
 #include<iostream>
+#include<vector>
+#include<iostream>
+#include<fstream>
 using namespace std;
-  
-// Keeps track of the number of processes
-const int numP = 5;
-  
-// Keeps track of the number of resources
-const int numR = 3;
-  
-// Function to find the needed resources of each process
-void calculateNeededResource(int needed[numP][numR], int maxR[numP][numR], int allotted[numP][numR])
+
+void getData(vector<vector<int>> &allocated, vector<vector<int>> &maxR, vector<int> &available, int &numP, int &numR)
 {
-  // Calculates the needed resources of each process
+  // Opens input file
+  ifstream in;
+  in.open("input.csv");
+
+  // Checks to see if file faild to open
+  if(!in) 
+  {
+    std::cerr << "File was unable to open..." << '\n';
+    exit(1);
+  }
+
+  char c;
+  int count = 0;
+  string str;
+  vector<int> row;
+
+  // While you haven't reached the end of the file
+  while(!in.eof()) 
+  {
+    // Reads in a character
+    in.get(c);
+    // Checks if the character is a letter
+    if(!isdigit(c)) 
+    { 
+      // Checks to see if the letter is blank
+      if(str != "") 
+      {
+        // Converts the letter to an int
+        int num = stoi(str);
+        if(count == 2) 
+        {
+          available.push_back(num);
+        }        
+        // Pushes the num value onto the row
+        row.push_back(num);
+      }
+      // Resets the value of the input
+      str = "";
+    }
+    // Adds the character to a string if it is a number
+    else if(isdigit(c) && c != ' ' && c != ',') 
+    {
+      str.push_back(c);
+    }
+    // Checks to see if there is a new line character
+    if(c == '\n' || c == '\t' || c == '\r') 
+    {
+      // Resets the count variable
+      count = 0;
+      // Clears row's values
+      row.clear();            
+    }
+    // Checks for a comma
+    else if(c == ',') 
+    {
+      // Checks if the rows input add to the allocated ones
+      if(count == 0) 
+      {
+        allocated.push_back(row);
+      }
+      // Checks if the rows input add to the max resources
+      else if(count == 1) 
+      {
+        maxR.push_back(row);
+      }
+      // Clears row's values
+      row.clear();
+      // Increments count
+      count++;
+    }
+  }
+  // Sets the number of processes based on allocated's size
+  numP = allocated.size();
+  // Sets the number of resources based on avaiable's size
+  numR = available.size();
+}
+
+// Function to find out if the system is in safe state or not
+void checkSafeState(vector<vector<int>> &allocated, vector<vector<int>> &maxR, vector<int> &available, int &numP, int &numR)
+{
+  // Holds the needed resources
+  int needed[numP][numR];
+  
+  // Used calculate needed resources
   for(int i = 0 ; i < numP ; i++)
   {
     for(int j = 0 ; j < numR ; j++)
     {
       // Needed resource = max resource - allocated resource
-      needed[i][j] = maxR[i][j] - allotted[i][j];
+      needed[i][j] = maxR[i][j] - allocated[i][j];
     }
   }
-}
-
-// Function to find out if the system is in safe state or not
-bool checkSafeState(int processes[], int available[], int maxR[][numR], int allotted[][numR])
-{
-  int needed[numP][numR];
-  
-  // Function to calculate needed resources
-  calculateNeededResource(needed, maxR, allotted);
   
   // Keeps track of which processes have finished
-  bool finished[numP] = {0};
+  bool finished[numP];
+  // Sets the finished array to 0
+  for(int i = 0; i < numP; i++){
+    finished[i] = 0;
+  }
   
   // Stores the safe sequence
   int safeSequence[numP];
-  
-  // Make a copy of available resources
-  int copyR[numR];
-  for (int i = 0; i < numR ; i++)
-  {
-    copyR[i] = available[i];
-  }
-  
-  // While all processes have not finished or the system is not in the safe state.
+
+  // Acts as a count for traversing safeSequence
   int count = 0;
-  while(count < numP)
-  {
-    // Finds a process which is not finished yet and whose needed resources can be satisfied with current copied resources.
-    bool truth = false;
-    for(int p = 0; p < numP; p++)
+
+  // Iterates through all the processes
+  for(int i = 0; i < numP; i++)
+  { 
+    // Iterates through all of the processes to allow for each possible senario
+    for(int j = 0; j < numP; j++)
     {
-      // Checks to see if a process is finished, if not then it continues
-      if(finished[p] == 0)
-      {
-        // Checks if for all resources of the current process, if needed resources is less than copy
-        int j;
-        for(j = 0; j < numR; j++)
+      // If a proceess has not finished yet
+      if(finished[j] == false)
+      { 
+        // Turns to true when able to add to safeSequence
+        int truth = false;
+        // Iterates through all the processes
+        for(int k = 0; k < numR; k++)
         {
-          if(needed[p][j] > copyR[j])
+          // If not able to complete the process
+          if(needed[j][k] > available[k])
           {
+            truth = 1;
             break;
           }
         }
-        // If all needs of the process were satisfied.
-        if(j == numR)
+        // If a process was able to finish
+        if(truth == false)
         {
-          // Add the allocated resources of current process to the available resources and free the resources afterward
-          for(int k = 0 ; k < numR ; k++)
-          {
-            copyR[k] += allotted[p][k];
+          // Adds that process to the safe sequence
+          safeSequence[count++] = j;
+          // Iterates through all resources
+          for(int l = 0; l < numR; l++){
+            // Adds the allocated resources from that process to the available
+            available[l] += allocated[j][l];
           }
-          // Add this process to safe sequence.
-          safeSequence[count++] = p;
-  
-          // Mark the current process as finished
-          finished[p] = 1;
-          truth = true;
+            // Sets that process to finished
+            finished[j] = true;
         }
       }
     }
-    // If we could not find a next process in safe sequence.
-    if(truth == false)
+  }
+  // Keeps track of if system is in safe state
+  int truth = true; 
+  // Iterates through all processes
+  for(int i = 0; i < numP; i++) 
+  {
+    // If process is unfinished
+    if(finished[i] == 0)
     {
-      cout << "System is not in safe state";
-      return false;
+      truth = 0;
     }
   }
-  // If system is in safe state then safe sequence will be as below
-  cout << "System is in safe state.\nSafe"
-         " sequence is: ";
-  for(int i = 0; i < numP ; i++)
+  // If unable to get a safe sequence
+  if(truth == false)
   {
-    cout << safeSequence[i] << " ";
+    cout << "System is not in safe state";
+    return;
   }
-  return true;
+  // If system is in a safe state
+  if(truth == true)
+  {
+    cout << "Safe sequence is: \n";
+    // Prints safe sequence
+    for(int i = 0; i < numP - 1; i++){
+      cout << safeSequence[i] << " "; 
+    }
+      cout << safeSequence[numP-1] << '\n';
+    }
 }
 
 // Main code
 int main()
 {
-  // Number of processes
-  int processes[] = {0, 1, 2, 3, 4};
-  
-  // Available instances of resources
-  int available[] = {3, 3, 2};
-  
-  // Max resources that can be allocated to each process
-  int maxR[][numR] = {{7, 5, 3}, {3, 2, 2}, {9, 0, 2}, {2, 2, 2}, {4, 3, 3}};
-  
-  // Resources allocated to processes
-  int allocated[][numR] = {{0, 1, 0}, {2, 0, 0}, {3, 0, 2}, {2, 1, 1}, {0, 0, 2}};
-  
-  // Check to see if system is in a safe state or not
-  checkSafeState(processes, available, maxR, allocated);
-  
+  // Holds the number of processes
+  int numP;
+  // Holds the number of resources
+  int numR; 
+  // Holds the allocated resources
+  vector<vector<int>> allocated;
+  // Holds the max resources
+  vector<vector<int>> maxR;
+  // Holds the available resources
+  vector<int> available;
+  // Takes data from input at stores it
+  getData(allocated, maxR, available, numP, numR);
+  // Checks to see if system is in a safe state or not
+  checkSafeState(allocated, maxR, available, numP, numR);
   return 0;
 }
